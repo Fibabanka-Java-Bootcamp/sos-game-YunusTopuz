d7d186618ae189e0004fcba602ff286b04abfbd5
Yunus Topuz <ytopuz@infina.com.tr>
2020-12-29 23:51:54 +0300

    Projenin tamamlanması

--- src/org/kodluyoruz/SosGame.java
+++ src/org/kodluyoruz/SosGame.java
@@ -1,144 +1,270 @@
 package org.kodluyoruz;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class SosGame {
 
-    static Scanner scan = new Scanner(System.in);
-    static String sosMatrix[][];
-    static int myScore;
-    static int pcScore;
+	static Scanner scan = new Scanner(System.in);
+	static String sosMatrix[][];
+	static int myScore;
+	static int pcScore;
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        int tableSize, row, column;
-        String  word;
-        Boolean checkEmpty;
-        do {
-            System.out.print("Tablonun boyutunu giriniz : ");
-            tableSize = scan.nextInt();
-            if(tableSize < 3 || tableSize > 7){
-                System.out.println("Lütfen 3x3 ile 7x7 arasýnda bi boyut giriniz. Ýþlem Tekrar yapýlýyor.");
-            }
-        }while (tableSize < 3 || tableSize > 7);
+		int tableSize, row, column;
+		String word;
+		Boolean checkEmpty;
+		do {
+			System.out.print("Tablonun boyutunu giriniz : ");
+			tableSize = scan.nextInt();
+			if (tableSize < 3 || tableSize > 7) {
+				System.out.println("Lütfen 3x3 ile 7x7 arasýnda bi boyut giriniz. Ýþlem Tekrar yapýlýyor.");
+			}
+		} while (tableSize < 3 || tableSize > 7);
 
-        sosMatrix = new String[tableSize][tableSize];
+		sosMatrix = new String[tableSize][tableSize];
 
-        for (int i = 0; i < sosMatrix.length; i++) {
-            for (int j = 0; j < sosMatrix[0].length; j++) {
-                sosMatrix[i][j]= "-";
-            }
-        }
+		for (int i = 0; i < sosMatrix.length; i++) {
+			for (int j = 0; j < sosMatrix[0].length; j++) {
+				sosMatrix[i][j] = "-";
+			}
+		}
 
-        printTable(sosMatrix);
+		printTable(sosMatrix);
 
-        while (!isGameOver()){
-            do {
-                System.out.print("Konum için satýrý giriniz. : ");
-                row = scanLocation();
-                System.out.print("Konum için sütunu giriniz. : ");
-                column = scanLocation();
-                System.out.print("S veya O harfi giriniz. : ");
-                word = scanWord();
-                checkEmpty = checkEmpty(row, column);
-                if (checkEmpty){
-                    sosMatrix[row][column] = word;
-                    printTable(sosMatrix);
-                }
-            } while (!checkEmpty);
-            do {
-                if (!isGameOver()){
-                    System.out.print("Bilgisayar Oynuyor");
-                    row = new Random().nextInt(sosMatrix.length);
-                    column = new Random().nextInt(sosMatrix[0].length);
-                    word = new Random().nextInt(2) == 0 ? "S" : "O";
-                                                                //Ýlk kimin baþlýyacagý random belli olmasý
-                    checkEmpty = checkEmpty(row, column);
-                    if (checkEmpty){
-                        System.out.println("Bilgisayar " + row + " " + column + " konumuna " + word + " karakterini yerleþtirdi.");
-                        sosMatrix[row][column] = word;
-                        printTable(sosMatrix);
-                    }
-                }
-            } while (!checkEmpty);
-        }
+		while (!isGameOver()) {
+			do {
+				System.out.print("Konum için satýrý giriniz. : ");
+				row = scanLocation();
+				System.out.print("Konum için sütunu giriniz. : ");
+				column = scanLocation();
+				System.out.print("S veya O harfi giriniz. : ");
+				word = scanWord();
+				checkEmpty = checkEmpty(row, column);
+				if (checkEmpty) {
+					sosMatrix[row][column] = word;
+					printTable(sosMatrix);
+					if (checkTable(row, column, word, 0)) {
+						System.err.println("SOS oldu. Ben bir puan aldým.");
+						System.err.println("Benim Puaným : " + myScore);
+						System.err.println("Bilgisayar Puaný : " + pcScore);
+					}
+				}
+			} while (!checkEmpty);
+			do {
+				if (!isGameOver()) {
+					System.out.print("Bilgisayar Oynuyor");
+					row = new Random().nextInt(sosMatrix.length);
+					column = new Random().nextInt(sosMatrix[0].length);
+					word = new Random().nextInt(2) == 0 ? "S" : "O";
+					checkEmpty = checkEmpty(row, column);
+					if (checkEmpty) {
+						System.out.println(" Bilgisayar " + row + " " + column + " konumuna " + word + " karakterini yerleþtirdi.");
+						sosMatrix[row][column] = word;
+						printTable(sosMatrix);
+						if (checkTable(row, column, word, 1)) {
+							System.err.println("SOS oldu. Bilgisayar bir puan aldý.");
+							System.err.println("Benim Puaným : " + myScore);
+							System.err.println("Bilgisayar Puaný : " + pcScore);
+						}
+					}
+				}
+			} while (!checkEmpty);
+		}
 
-        printTable(sosMatrix);
+		printTable(sosMatrix);
 
-        System.out.println("Benim Puaným : " + myScore);
-        System.out.println("Bilgisayar Puaný : " + pcScore);
+		System.err.println("Benim Puaným : " + myScore);
+		System.err.println("Bilgisayar Puaný : " + pcScore);
 
-        System.out.println(myScore > pcScore ? "Kazanan Benim" : "Kazanan Bilgisayar");
-    }
+		System.err.println(myScore > pcScore ? "Kazanan Benim" : "Kazanan Bilgisayar");
+	}
 
-    /*public static void checkRow (String[][] sosMatrix, int x, int y ,char deger) {
-        x<2 y<2 ve y>5 puan kontrolüne girmez
-        x>5 ise y<2 ve y>5 puan kontrolüne gimrez.
-        if(x < sosMatrix.length-1 && x>1){
-            if(y<sosMatrix[0].length-1 && y >1){
-                y-1 ken x her durum
-                        y+1 ken her durum
-                            y sabitken x-1 ve x+1
-            }
-            else if(y<=1){
-               y sabitken x -1 ve +1 olacak,
-                y+1 ken x her durum
-            }
-            else
-                y sabitken x-1 ve x+1
-                    y-1 ken x her durum
-        }
-4
-    }*/
+	public static boolean checkTable(int row, int column, String value, int order) {
+		boolean successful = false;
+		if (value == "O") {
+			if (row - 1 >= 0 && column + 1 < sosMatrix.length) {
+				if (((sosMatrix[row - 1][column]) + (sosMatrix[row][column]) + (sosMatrix[row + 1][column])).equals("SOS")) {
+					if (order == 0) {
+						myScore++;
+						successful = true;
+					} else {
+						pcScore++;
+						successful = true;
+					}
+				}
+			}
 
-    public static boolean isGameOver (){
-        for (int i = 0; i < sosMatrix.length; i++) {
-            for (int j = 0; j < sosMatrix[0].length; j++) {
-                if (sosMatrix[i][j] == "-" ){
-                    System.out.println("oyun bitmedi");
-                    return false;
-                }
-            }
-        }
-        System.out.println("Oyun Bitti.");
-        return true;
-    }
+			if (column - 1 >= 0 && column + 1 < sosMatrix.length) {
+				if (((sosMatrix[row][column - 1]) + (sosMatrix[row][column]) + (sosMatrix[row][column + 1])).equals("SOS")) {
+					if (order == 0) {
+						myScore++;
+						successful = true;
+					} else {
+						pcScore++;
+						successful = true;
+					}
+				}
+			}
+			if ((column - 1 >= 0 && row - 1 >= 0) && (column + 1 < sosMatrix.length && row + 1 < sosMatrix.length)) {
+				if (((sosMatrix[row - 1][column - 1]) + (sosMatrix[row][column]) + (sosMatrix[row + 1][column + 1])).equals("SOS")) {
+					if (order == 0) {
+						myScore++;
+						successful = true;
+					} else {
+						pcScore++;
+						successful = true;
+					}
+				}
+			}
 
-    public static boolean checkEmpty (int row, int column) {
-        if (row < 0 || sosMatrix.length <= row || sosMatrix[0].length <= column || column < 0) {
-            System.out.println("*****************");
-            System.out.println("Girdiðiniz deðerler tabloyu aþtý. Tekrar giriniz.");
-            return false;
-        }else {
-            System.out.println("-------------");
-            System.out.println(sosMatrix[row][column] + " " + row + " " + column);
-            if (sosMatrix[row][column] != "-") {
-                System.out.println("//////////////////");
-                System.out.println("Girdiðiniz konum doludur. Lütfen boþ alaný seçiniz. Ýþlem tekrar yapýlýyor.");
-                return false;
-            }
-        }
-        return true;
-    }
+			if ((row - 1 >= 0 && column + 1 < sosMatrix.length) && (column - 1 >= 0 && row + 1 < sosMatrix.length)) {
+				if (((sosMatrix[row - 1][column + 1]) + (sosMatrix[row][column]) + (sosMatrix[row + 1][column - 1])).equals("SOS")) {
+					if (order == 0) {
+						myScore++;
+						successful = true;
+					} else {
+						pcScore++;
+						successful = true;
+					}
+				}
+			}
+		} else {
+			if (row - 2 >= 0) {
+				if (((sosMatrix[row - 2][column]) + (sosMatrix[row - 1][column]) + (sosMatrix[row][column])).equals("SOS")) {
+					if (order == 0) {
+						myScore++;
+						successful = true;
+					} else {
+						pcScore++;
+						successful = true;
+					}
+				}
+				if (column - 2 >= 0) {
+					if (((sosMatrix[row - 2][column - 2]) + (sosMatrix[row - 1][column - 1]) + (sosMatrix[row][column])).equals("SOS")) {
+						if (order == 0) {
+							myScore++;
+							successful = true;
+						} else {
+							pcScore++;
+							successful = true;
+						}
+					}
+				}
+			}
+			if (row + 2 < sosMatrix.length) {
+				if (((sosMatrix[row][column]) + (sosMatrix[row + 1][column]) + (sosMatrix[row + 2][column])).equals("SOS")) {
+					if (order == 0) {
+						myScore++;
+						successful = true;
+					} else {
+						pcScore++;
+						successful = true;
+					}
+				}
+				if (column + 2 < sosMatrix.length) {
+					if (((sosMatrix[row][column]) + (sosMatrix[row + 1][column + 1]) + (sosMatrix[row + 2][column + 2])).equals("SOS")) {
+						if (order == 0) {
+							myScore++;
+							successful = true;
+						} else {
+							pcScore++;
+							successful = true;
+						}
+					}
+				}
+			}
+			if (column - 2 >= 0) {
+				if (((sosMatrix[row][column - 2]) + (sosMatrix[row][column - 1]) + (sosMatrix[row][column])).equals("SOS")) {
+					if (order == 0) {
+						myScore++;
+						successful = true;
+					} else {
+						pcScore++;
+						successful = true;
+					}
+				}
+				if (row + 2 < sosMatrix.length) {
+					if (((sosMatrix[row][column]) + (sosMatrix[row + 1][column - 1]) + (sosMatrix[row + 2][column - 2])).equals("SOS")) {
+						if (order == 0) {
+							myScore++;
+							successful = true;
+						} else {
+							pcScore++;
+							successful = true;
+						}
+					}
+				}
+			}
+			if (column + 2 < sosMatrix.length) {
+				if (((sosMatrix[row][column]) + (sosMatrix[row][column + 1]) + (sosMatrix[row][column + 2])).equals("SOS")) {
+					if (order == 0) {
+						myScore++;
+						successful = true;
+					} else {
+						pcScore++;
+						successful = true;
+					}
+				}
+				if (row - 2 >= 0) {
+					if (((sosMatrix[row][column]) + (sosMatrix[row - 1][column + 1]) + (sosMatrix[row - 2][column + 2])).equals("SOS")) {
+						if (order == 0) {
+							myScore++;
+							successful = true;
+						} else {
+							pcScore++;
+							successful = true;
+						}
+					}
+				}
+			}
+		}
+		return successful;
+	}
 
-    public static void printTable(String[][] sosMatrix) {
-        System.out.println();
-        for (int i = 0; i < sosMatrix.length; i++) {
-            for (int j = 0; j < sosMatrix[0].length; j++) {
-                System.out.print(sosMatrix[i][j]);
-                System.out.print("   ");
-            }
-            System.out.println("");
-        }
-    }
+	public static boolean isGameOver() {
+		for (int i = 0; i < sosMatrix.length; i++) {
+			for (int j = 0; j < sosMatrix[0].length; j++) {
+				if (sosMatrix[i][j] == "-") {
+					return false;
+				}
+			}
+		}
+		System.out.println("Oyun Bitti.");
+		return true;
+	}
 
-    public static int scanLocation(){
-        return scan.nextInt();
-    }
+	public static boolean checkEmpty(int row, int column) {
+		if (row < 0 || sosMatrix.length <= row || sosMatrix[0].length <= column || column < 0) {
+			System.out.println("Girdiðiniz deðerler tabloyu aþtý. Girdi 0 ile " + String.valueOf(sosMatrix.length -1) + " arasýnda olmalýdýr. Tekrar giriniz.");
+			return false;
+		} else {
+			if (sosMatrix[row][column] != "-") {
+				System.out.println("Girdiðiniz konum doludur. Lütfen boþ alaný seçiniz. Ýþlem tekrar yapýlýyor.");
+				return false;
+			}
+		}
+		return true;
+	}
 
-    public static String scanWord(){
-        return scan.next();
-    }
+	public static void printTable(String[][] sosMatrix2) {
+		System.out.println();
+		for (int i = 0; i < sosMatrix2.length; i++) {
+			for (int j = 0; j < sosMatrix2[0].length; j++) {
+				System.out.print(sosMatrix2[i][j]);
+				System.out.print("   ");
+			}
+			System.out.println("");
+		}
+	}
 
+	public static int scanLocation() {
+		return scan.nextInt();
+	}
+
+	public static String scanWord() {
+		return scan.next();
+	}
 
 }
